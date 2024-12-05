439780a2cc5673ce09b1b10acacbf6bd2991bcb6
Ryan Ptomey <rptomey6@gmail.com>
2024-12-05 10:02:20 -0600

    part 2

--- day05_page-order/aoc24_05.py
+++ day05_page-order/aoc24_05.py
@@ -40,16 +40,22 @@ def part1(data):
     rules = data["rules"]
     updates = data["updates"]
 
+    # Prep for part2
+    data["failures"] = set()
+
     for rule in rules.keys():
         rules[rule]["regex"] = re.compile(f"(?:{'|'.join(rules[rule]["numbers"])}).*{rule}")
 
     for update in updates:
+        relevant_rules = set(rules.keys()).intersection(update)
         update_string = ",".join(update)
         checking = True
         safe = True
         while checking:
-            for rule in rules.keys():
+            for rule in relevant_rules:
+            # for rule in rules.keys():
                 if re.search(rules[rule]["regex"], update_string):
+                    data["failures"].add(update_string)
                     safe = False
                     checking = False
             checking = False
@@ -62,7 +68,31 @@ def part1(data):
 
 def part2(data):
     """Solve part 2."""
-    pass
+    total = 0
+    rules = data["rules"]
+    failures = data["failures"]
+
+    for update_string in failures:
+        update = update_string.split(",")
+        relevant_rules = set(rules.keys()).intersection(update)
+        rules_to_satisfy = list(relevant_rules)
+        while len(rules_to_satisfy) > 0:
+            rule_id = rules_to_satisfy.pop(0)
+            relevant_numbers = set(rules[rule_id]["numbers"]).intersection(update)
+            if len(relevant_numbers) > 0:
+                id_index = update.index(rule_id)
+                first_relevant_index = min([update.index(num) for num in relevant_numbers])
+                if id_index > first_relevant_index:
+                    num = update.pop(id_index)
+                    update.insert(first_relevant_index, num)
+                    rules_to_satisfy.append(rule_id)
+
+        
+        middle_index = math.floor(len(update)/2)
+        middle = int(update[middle_index])
+        total += middle
+    
+    return total
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
